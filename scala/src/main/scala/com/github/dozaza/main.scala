af9bc0b170bf2bc7410bfee8cfd6adf06228729c
baoan.feng <baoan.feng@datayes.com>
2018-04-03 12:18:15 +0800

    finish question 7

--- scala/src/main/scala/com/github/dozaza/main.scala
+++ scala/src/main/scala/com/github/dozaza/main.scala
@@ -3,10 +3,36 @@ package com.github.dozaza
 object main {
 
   def main(args: Array[String]): Unit = {
-    println({
-      val q1 = Question1.twoSum(Array(2, 7, 11, 15), 9)
-      q1(0) + ", " + q1(1)
-    })
+    // Question 1
+//    println({
+//      val q1 = Question1.twoSum(Array(2, 7, 11, 15), 9)
+//      q1(0) + ", " + q1(1)
+//    })
+
+    // Question7
+    println(Question7.reverse(Int.MinValue) + ", " + Question7.reverse(321))
+  }
+
+  object Question7 {
+    def reverse(x: Int): Int = {
+      val str = x.toString
+      val reversed = if (x < 0) str.substring(1).reverse.toLong else str.reverse.toLong
+      if (x < 0) {
+        if (reversed > Int.MaxValue) {
+          0
+        } else {
+          reversed.toInt * -1
+        }
+      } else if (x > 0) {
+        if (reversed > Int.MaxValue) {
+          0
+        } else {
+          reversed.toInt
+        }
+      } else {
+        reversed.toInt
+      }
+    }
   }
 
   object Question1 {
