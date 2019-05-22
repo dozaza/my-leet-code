233ab15d276532ccf1d2a6e5579ed6dd2586ff48
dozaza <baoan.feng@gmail.com>
2019-05-22 22:00:16 +0800

    fix Question 10

--- src/main/java/com/github/dozaza/Question_0010.java
+++ src/main/java/com/github/dozaza/Question_0010.java
@@ -63,29 +63,56 @@ package com.github.dozaza;
 public class Question_0010 {
 
     public static void main(String[] args) {
-        int x = 121;
-        boolean res = isPalindrome(x);
+        String s = "aab";
+        String p = "*c";
+        boolean res = isMatch(s, p);
         System.out.println(res);
     }
 
     public static boolean isMatch(String s, String p) {
-        // Dynamic Programming
+        /**
+         * 1. p[j] == s[i] || p[j] == '.':
+         *      dp[i][j] = dp[i-1][j-1]
+         * 2. p[j] == '*':
+         *  2.1 p[j-1] != s[i]:
+         *      dp[i][j] = dp[i][j-2]
+         *  2.2 p[i-1] == s[i] or p[i-1] == '.':
+         *      dp[i][j] = dp[i-1][j] // 多个a的情况
+         *      || dp[i][j-1] // 单个a的情况
+         *      || dp[i][j-2] // 没有a的情况
+         */
         int sLen = s.length();
         int pLen = p.length();
         char[] sArray = s.toCharArray();
         char[] pArray = p.toCharArray();
-        boolean[][] memory = new boolean[sLen+1][pLen+1];
-        memory[0][0] = true;
-        for(int i = 0; i <= sLen; i++) {
-            for(int j = 1; j <= pLen; j++) {
-                if (pArray[j-1] == '*') {
-                    memory[i][j] = memory[i][j-2] || (i > 0 && (sArray[i-1] == pArray[j-2] || pArray[j-2] == '.') && memory[i-1][j]);
-                } else {
-                    memory[i][j] = i > 0 && (sArray[i-1] == pArray[j-1] || pArray[j-1] == '.') && memory[i-1][j-1];
+        boolean[][] dp = new boolean[sLen+1][pLen+1];
+
+        dp[0][0] = true;
+        for (int i = 0; i < pLen; i++) {
+            if (pArray[i] == '*' && (i == 0 || dp[0][i - 1])) {
+                dp[0][i + 1] = true;
+            }
+        }
+
+        for (int i = 0; i < sLen; i++) {
+            for (int j = 0; j < pLen; j++) {
+                if (pArray[j] == sArray[i] || pArray[j] == '.') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else if (pArray[j] == '*'){
+                    if (j == 0) {
+                        dp[i][j] = true;
+                    } else {
+                        if (pArray[j - 1] != sArray[i]) {
+                            dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                        } else if (pArray[i - 1] == sArray[i] || pArray[j] == '.') {
+                            dp[i + 1][j + 1] = dp[i][j+1] || dp[i + 1][j] || dp[i + 1][j - 1];
+                        }
+                    }
                 }
             }
         }
-        return memory[sLen][pLen];
+
+        return dp[sLen][pLen];
     }
 
 }
