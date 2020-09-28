0abfb0f49edff198fecc130c47be601cca74aeed
dozaza <baoan.feng@gmail.com>
2020-09-28 09:47:43 +0800

    update question 283

--- src/main/python/question_0283.py
+++ src/main/python/question_0283.py
@@ -26,12 +26,14 @@ class Solution:
         if len(nums) <= 1:
             return
 
+        # j: idx of 0, i: idx of none-0
         j = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 if i != j:
                     nums[i], nums[j] = nums[j], nums[i]
                 j += 1
+
 
 if __name__ == '__main__':
     s = Solution()
