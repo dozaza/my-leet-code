4a81732cc9973ae6c9c8b91e95b2325af080542a
dozaza <baoan.feng@gmail.com>
2020-10-28 22:45:57 +0800

    add question 344

---
+++ src/main/python/question_0344.py
@@ -0,0 +1,48 @@
+# 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。 
+# 
+#  不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+# 
+#  你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入：["h","e","l","l","o"]
+# 输出：["o","l","l","e","h"]
+#  
+# 
+#  示例 2： 
+# 
+#  输入：["H","a","n","n","a","h"]
+# 输出：["h","a","n","n","a","H"] 
+#  Related Topics 双指针 字符串 
+#  👍 319 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        if len(s) <= 1:
+            return
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+
+if __name__ == '__main__':
+    s = Solution()
+    a = ["h","e","l","l","o"]
+    s.reverseString(a)
+    print(a)
+    a = ["H","a","n","n","a","h"]
+    s.reverseString(a)
+    print(a)
+# leetcode submit region end(Prohibit modification and deletion)
